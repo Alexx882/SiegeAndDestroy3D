ca116a0cb3c6a04f491a5cb08840e4f7360cbf78
pareinwald <p.reinwald@gmx.at>
2018-04-05 17:41:10 +0200

    SiegeAndDestroy3D-38 NewGameScreen

---
+++ app/src/main/java/at/aau/gloryweapons/siegeanddestroy3d/NewGameActivity.java
@@ -0,0 +1,25 @@
+package at.aau.gloryweapons.siegeanddestroy3d;
+
+import android.net.wifi.WifiManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.format.Formatter;
+import android.widget.TextView;
+
+public class NewGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_game);
+
+        WifiManager wm = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
+        String ip = Formatter.formatIpAddress(wm.getConnectionInfo().getIpAddress());
+
+        TextView t = new TextView(this);
+
+        t=(TextView)findViewById(R.id.textViewIpHost);
+        t.setText(ip);
+
+    }
+}
